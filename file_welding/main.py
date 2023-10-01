2b1ff671efd6f8b4253c4cc49f420a505022c01c
Liam Gibbons <112109780+lgibbons12@users.noreply.github.com>
2023-09-30 23:25:57 -0400

    Update main.py

--- file_welding/main.py
+++ file_welding/main.py
@@ -8,10 +8,20 @@ df = pd.read_csv("illinois_county_level_data_2018_2022_combine.csv")
 
 def weld_dataframes(df):
     df = count_seat.add_seats(df)
+    print("County Seats sucessfully welded")
+    print()
     df = latlng.get_latlng(df)
+    print("Latitude and Longitude sucessfully welded")
+    print()
     df = population.get_population(df)
+    print("Population sucessfully welded")
+    print()
     df = fips.get_fips(df)
+    print("FIPS ID sucessfully welded")
+    print()
     df = phylodiversity.get_phylo(df)
+    print("Phylodiversity sucessfully welded")
+    print()
 
     return df
 
@@ -19,11 +29,17 @@ def weld_dataframes(df):
 df = weld_dataframes(df)
 
 df = time_add.add_time(df)
+print("Time successfully welded")
+print()
 
 df = final.do(df)
+print("Monthly Average Climate Data sucessfully welded")
+print()
  
 
 df = extremes.do(df)
+print("Hourly Extreme Climate Data sucessfully welded")
+print()
 
 df.drop("keys", inplace = True, axis = 1)
 
